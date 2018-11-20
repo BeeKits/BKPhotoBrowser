3652200487d84eb1b278963e675c494870490fba
LinXunFeng <598600855@qq.com>
2018-11-20 15:11:11 +0800

    Initial Commit : )

---
+++ BKPhotoBrowser/BKPhotoBrowser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BKPhotoBrowser
+//
+//  Created by LinXunFeng on 2018/11/20.
+//  Copyright © 2018 LinXunFeng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ BKPhotoBrowser/BKPhotoBrowserTests/BKPhotoBrowserTests.swift
@@ -0,0 +1,34 @@
+//
+//  BKPhotoBrowserTests.swift
+//  BKPhotoBrowserTests
+//
+//  Created by LinXunFeng on 2018/11/20.
+//  Copyright © 2018 LinXunFeng. All rights reserved.
+//
+
+import XCTest
+@testable import BKPhotoBrowser
+
+class BKPhotoBrowserTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
